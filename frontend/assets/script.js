a8acf1e9cc2d9a656f7253e43753be1e196d2e3f
Niklas Kleiser <nk-213780@rwu.de>
2023-04-28 18:27:08 +0200

    added eslint and prettier

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -1,25 +1,21 @@
-
-
 function isValidEmail(email) {
-   
-    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-
-    return emailPattern.test(email);
+  return emailPattern.test(email);
 }
 
 function sendData() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let data = {
-        email: email,
-        name: name
-      }
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let data = {
+    email: email,
+    name: name,
+  };
 
-    if (name == "" || email == "") {
-        alert("Bitte geben Sie Ihren Namen und Ihre E-Mail-Adresse ein!");
-        return;
-    }
+  if (name == "" || email == "") {
+    alert("Bitte geben Sie Ihren Namen und Ihre E-Mail-Adresse ein!");
+    return;
+  }
 
   if (!isValidEmail(email)) {
     alert("Bitte geben Sie eine gültige E-Mail-Adresse ein!");
@@ -28,17 +24,17 @@ function sendData() {
     alert("Du hast dich erfolgreich angemeldet!");
   }
 
+  console.log(data);
 
-    console.log(data)
-
-    axios.post('https://dbaccessfunction.azurewebsites.net/api/attendee', JSON.stringify(data))
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
+  axios
+    .post(
+      "https://dbaccessfunction.azurewebsites.net/api/attendee",
+      JSON.stringify(data)
+    )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
-
-
