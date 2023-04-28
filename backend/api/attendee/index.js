649c6bcc121f1550486211e8902e985a98fe01ac
David Metzler <dm-213782@hswgt.hs-weingarten.de>
2023-04-28 18:39:15 +0200

    Prettify backend

--- backend/api/attendee/index.js
+++ backend/api/attendee/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
 
     let user = req.body;
 
-    
+
     const url = process.env.mongoConnectionString;
     context.log("Env: " + url);
     const mongo = new MongoClient(url);
@@ -13,10 +13,10 @@ module.exports = async function (context, req) {
     const db = mongo.db("event");
     const collection = db.collection('attendees');
 
-    const query = { name: user.name};
+    const query = { name: user.name };
     const update = { $set: user };
-    const options = {upsert: true, new: true};
-    
+    const options = { upsert: true, new: true };
+
     const upsertResult1 = await collection.updateOne(query, update, options);
     context.log(`upsertResult1: ${JSON.stringify(upsertResult1)}\n`);
 
